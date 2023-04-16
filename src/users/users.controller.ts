2e8925cad03a1e20492c87199b4651e1e8d2d0fb
sirawit.mahanin <sirawit.mn@gmail.com>
2023-04-16 22:43:09 +0700

    First repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Patch } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private userService: UsersService) {
+
+    }
+
+    @Post('signup')
+    createUser(@Body() body: CreateUserDto) {
+        return this.userService.create(body.email, body.password);
+    }
+
+    @Patch('')
+    updateUser(@Body() body: CreateUserDto) {
+        return this.userService.create(body.email, body.password);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) {
+    }
+
+    create(email: string, password: string) {
+        const user: User = this.repo.create();
+        user.email = email;
+        user.password = password;
+
+        return this.repo.save(user);
+    }
+}
